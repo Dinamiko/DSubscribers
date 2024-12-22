7f7265aca964ae37d41a0c01e587adb9dfd9da7f
Emili Castells Guasch <e.castells@inpsyde.com>
2024-12-22 09:48:07 +0100

    Add unsubscribe test

--- tests/acceptance/user-subscribe.test.js
+++ tests/acceptance/user-subscribe.test.js
@@ -1,4 +1,31 @@
-import {test, expect} from '@playwright/test'
+import {test, expect} from '@playwright/test';
+
+test('user subscribe', async ({page}) => {
+    const name = crypto.randomUUID();
+    await subscribe(page, name);
+
+    await loginAdmin(page);
+
+    await expect(page.getByText(name)).toBeVisible();
+});
+
+test('user unsubscribe', async ({page}) => {
+    const name = crypto.randomUUID();
+    await subscribe(page, name);
+
+    await page.locator('#form-validation-unsubscribe').getByPlaceholder('E-mail').fill(`${name}@example.com`);
+    await page.locator('#form-validation-unsubscribe').getByRole('button', {name: 'SUBMIT'}).click();
+
+    await loginAdmin(page);
+
+    await expect(page.getByText(name)).not.toBeVisible();
+});
+
+const subscribe = async (page, name) => {
+    await page.goto('/');
+    await page.locator('#form-validation').getByPlaceholder('E-mail').fill(`${name}@example.com`);
+    await page.locator('#form-validation').getByRole('button', {name: 'SUBMIT'}).click();
+}
 
 const loginAdmin = async (page) => {
     await page.goto('/wp-admin');
@@ -7,30 +34,3 @@ const loginAdmin = async (page) => {
     await page.getByRole('button', {name: 'Log In'}).click();
     await page.getByRole('link', {name: 'DSubscribers', exact: true}).click();
 }
-
-const subscribe = async (page, name) => {
-    await page.goto('/')
-    await page.locator('#form-validation').getByPlaceholder('E-mail').fill(`${name}@example.com`);
-    await page.locator('#form-validation').getByRole('button', {name: 'SUBMIT'}).click();
-}
-
-test('user subscribe', async ({page}) => {
-    const name = crypto.randomUUID()
-    await subscribe(page, name);
-
-    await loginAdmin(page);
-
-    await expect(page.getByText(name)).toBeVisible()
-})
-
-test('user unsubscribe', async ({page}) => {
-    const name = crypto.randomUUID()
-    await subscribe(page, name);
-
-    await page.locator('#form-validation-unsubscribe').getByPlaceholder('E-mail').fill(`${name}@example.com`);
-    await page.locator('#form-validation-unsubscribe').getByRole('button', {name: 'SUBMIT'}).click();
-
-    await loginAdmin(page);
-
-    await expect(page.getByText(name)).not.toBeVisible()
-})
